aa744132120e3749883335ab20b2bd7dfd17214e
Marcell Jonathan <57036356+marcelljoe@users.noreply.github.com>
2020-01-20 09:11:14 +0700

    Update EventsByCategoryActions.js

--- src/Redux/_actions/EventsByCategoryActions.js
+++ src/Redux/_actions/EventsByCategoryActions.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const getEventsByCategory = (id) => {
   return {
     type: "GET_EVENTS_BC",
-    payload: axios.get(`http://localhost:7000/dumbtick/category/${id}/events`)
+    payload: axios.get(`https://dumbtick-backend.herokuapp.com/dumbtick/category/${id}/events`)
   };
 };
